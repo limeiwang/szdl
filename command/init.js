c32da1de4b51995f3d2f1754adebf38a87245522
李美旺 <17803125899@163.com>
2024-02-06 09:50:09 +0800

    feat: 修改项目地址

--- command/init.js
+++ command/init.js
@@ -1,6 +1,6 @@
 'use strict'
 const exec = require('child_process').exec
-const projectUrl = 'https://github.com/limeiwang/szdl-vite.git'
+const projectUrl = 'https://github.com/limeiwang/szdl-cli.git'
 
 module.exports = () => {
     let cmdStr = `git clone `+projectUrl

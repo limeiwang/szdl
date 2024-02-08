8204cb707ba99788a654a9357fa864e01297b441
李美旺 <17803125899@163.com>
2024-02-08 09:30:20 +0800

    github link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,16 @@
 // .vitepress/config.js
 export default {
   // 站点级选项
-  title: 'szdl',
+  title: 'Szdl',
   description: 'Just playing around.',
 
   themeConfig: {
     // 主题级选项
     nav: [
-      { text: '首页', link: '/' },
-    ]
+      { text: '文档', link: '/' },
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/limeiwang/szdl-cli' },
+    ],
   }
 }
